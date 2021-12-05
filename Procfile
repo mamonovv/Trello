815e4287126dac572f57169a41d6bc21366d8f77
Vlad <mmvvlad@gmail.com>
2021-12-05 23:07:51 +0700

    Diploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Trell.wsgi --log-file -
--- Trell/settings.py
+++ Trell/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj-database-url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-ekfg0=e7m_m3d@e2a_i0d(f(8&*!nd)4jd#fk0bq@mmie16vzp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,3 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+db_from_env = dj-database-url.config()
+DATABASE['default'].update(db_from_env)
